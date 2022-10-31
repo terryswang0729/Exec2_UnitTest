b9e732e72c2cdb52c92df320a91297166120863e
士允 王 <terryswang0729@gmail.com>
2022-10-31 21:21:23 +0800

    加入專案檔案。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaiwanStock.Utilities;
+
+namespace ConsoleApp1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			
+			Console.WriteLine("請輸入交易日期，例如2022/10/31 9:00:");
+			
+			string input=Console.ReadLine();
+			bool isDate=DateTime.TryParse(input, out DateTime dt);
+			if(isDate==false)
+			{
+				Console.WriteLine("請輸入正確日期格式");
+				return;	
+			}	
+			var tradeDay = new TaiwanStockUtility().IsTradingTime(dt);
+			Console.WriteLine($"是否為交易時間:{tradeDay}");
+			
+
+
+
+		}
+	}
+}
---
+++ TaiwanStock.Utilities/TaiwanStockUtility.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaiwanStock.Utilities
+{
+	public class TaiwanStockUtility
+	{
+		public bool IsTradingTime(DateTime dt)
+		{
+			
+			int dayOfWeek=(int)dt.DayOfWeek;
+		    int tradeEndDay = 5, tradeStartDay = 1;
+			TimeSpan tradeTime=dt.TimeOfDay;
+			TimeSpan tradeTimeStart = new TimeSpan(9, 0, 0);
+			TimeSpan tradeTimeEnd = new TimeSpan(13,30, 0);
+			if (dayOfWeek > tradeEndDay||dayOfWeek<tradeStartDay) 
+			{
+				return false;
+			}
+			else if(tradeTime < tradeTimeStart || tradeTime > tradeTimeEnd)
+			{
+				return false;
+			}
+			else
+			{
+				return true;
+			}
+		}
+	
+	
+		 
+	}
+}
